81304afa9bdf47cdf910d02add02facca5c237e9
Adam <AdamVanHal@gmail.com>
2020-10-11 18:45:43 -0400

    Separated datetime and coords to 2 lines increase size of outline turned on AntiAliasing

--- src/gui/AppWindow.java
+++ src/gui/AppWindow.java
@@ -6,6 +6,7 @@ import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.Shape;
 import java.awt.font.FontRenderContext;
 import java.awt.font.TextLayout;
@@ -16,7 +17,12 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.text.DateFormat;
 import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -50,6 +56,8 @@ import org.apache.commons.imaging.formats.tiff.constants.TiffTagConstants;
 import org.apache.commons.imaging.formats.tiff.write.TiffOutputDirectory;
 import org.apache.commons.imaging.formats.tiff.write.TiffOutputField;
 import org.apache.commons.imaging.formats.tiff.write.TiffOutputSet;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 
 public class AppWindow {
 
@@ -93,6 +101,7 @@ public class AppWindow {
 		File file = null;
 		//fc.setMultiSelectionEnabled(true);
 		//TODO rewrite to iterate an array of files
+		//set what types of files to display in chooser
 		fc.setFileFilter(new FileFilter(){
 			public boolean accept(File file) {
 				if(file.getName().toLowerCase().endsWith(".jpg") || file.getName().toLowerCase().endsWith(".jpeg")) {
@@ -157,19 +166,32 @@ public class AppWindow {
 		}
         //Construct GPS Coord String
         DecimalFormat df = new DecimalFormat("0.00000");
-        String dateCoords = jpegMetadata.findEXIFValueWithExactMatch(TiffTagConstants.TIFF_TAG_DATE_TIME).getValueDescription();
+        String coords = null;
         try {
-			dateCoords = df.format(Math.abs(gpsInfo.getLatitudeAsDegreesNorth())) + "° " + gpsInfo.latitudeRef + ", " + df.format(Math.abs(gpsInfo.getLongitudeAsDegreesEast())) + "° " + gpsInfo.longitudeRef + " " + dateCoords;
+			coords = df.format(Math.abs(gpsInfo.getLatitudeAsDegreesNorth())) + "° " + gpsInfo.latitudeRef + ", " + df.format(Math.abs(gpsInfo.getLongitudeAsDegreesEast())) + "° " + gpsInfo.longitudeRef;
 		} catch (ImageReadException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+        //construct the date string
+        DateFormat dateFormat = new SimpleDateFormat("''yyyy:MM:dd HH:mm:ss''", Locale.ENGLISH);
+        DateFormat dateRenderFormat = new SimpleDateFormat("dd MMM yyyy h:mm a", Locale.ENGLISH);
+        Date imageDate = null;
+        try {
+			imageDate = dateFormat.parse(jpegMetadata.findEXIFValueWithExactMatch(TiffTagConstants.TIFF_TAG_DATE_TIME).getValueDescription());
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        String date = dateRenderFormat.format(imageDate);
+        
         
         //print gps and date info to SYSO for debugging reasons, remove in final release. TODO remove when done
         System.out.println(jpegMetadata.findEXIFValueWithExactMatch(TiffTagConstants.TIFF_TAG_DATE_TIME).getValueDescription());
+        System.out.println(date);
         System.out.println(gpsInfo.latitudeDegrees + "° " + gpsInfo.latitudeMinutes + "\' " + gpsInfo.latitudeSeconds.doubleValue() + "\" " + gpsInfo.latitudeRef);
         System.out.println(gpsInfo.longitudeDegrees + "° " + gpsInfo.longitudeMinutes + "\' " + gpsInfo.longitudeSeconds.doubleValue() + "\" " + gpsInfo.longitudeRef);
-        System.out.println(dateCoords);
+        System.out.println(coords);
         
         
         
@@ -188,23 +210,47 @@ public class AppWindow {
 //        TiffOutputField field = gpsCoords.findField(2);
         
         //setup font stuff so we can write on the image
-        Font overlayFont = new Font(Font.SANS_SERIF,Font.PLAIN,60);
+        Font overlayFont = new Font(Font.SANS_SERIF,Font.BOLD,70);
         gImage.setFont(overlayFont);
+        //suggest that it use antiAliaing since this is text
+        gImage.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        gImage.setRenderingHint(RenderingHints.KEY_RENDERING,
+                RenderingHints.VALUE_RENDER_QUALITY);
+        //draw date
         //create shape of the outer edge of the text. This will allow us to draw the outline in one color and then fill with solid color
-        TextLayout coordLayout = new TextLayout(dateCoords,overlayFont,new FontRenderContext(null,false,false)); //turn string into style we want for rendering
+        TextLayout textLayout = new TextLayout(date,overlayFont,new FontRenderContext(null,false,false)); //turn string into style we want for rendering
         AffineTransform transform = new AffineTransform();
-        transform.translate(image.getWidth()*0.03, image.getHeight()*0.95-coordLayout.getAscent());//set location of outline for rendering in the bottom of the image
-        Shape outline = coordLayout.getOutline(transform); //get the outer edges of the text
+        double startPoint = image.getHeight()*0.97 - textLayout.getAscent()*3.3;//where the line of text should be, start at 3 lines above the 97% mark 
+        transform.translate(image.getWidth()*0.03, startPoint);//set location of outline for rendering in the bottom of the image
+        startPoint = startPoint + textLayout.getAscent()*1.1;//drop down a line
+        Shape outline = textLayout.getOutline(transform); //get the outer edges of the text
         //Draw a string on the image
         //gImage.drawString(coords, 100, 100);//string does not quite line up if you use this, using fill instead.
         gImage.setColor(Color.BLACK);
-        gImage.setStroke(new BasicStroke(2));
+        gImage.setStroke(new BasicStroke(10));
+        gImage.draw(outline);//adds the outline
+        gImage.setColor(Color.YELLOW);
+        gImage.fill(outline); //fill in the outline with color
+        
+        //draw coords
+        //create shape of the outer edge of the text. This will allow us to draw the outline in one color and then fill with solid color
+        textLayout = new TextLayout(coords,overlayFont,new FontRenderContext(null,false,false)); //turn string into style we want for rendering
+        transform = new AffineTransform();
+        transform.translate(image.getWidth()*0.03, startPoint);//set location of outline for rendering in the bottom of the image
+        startPoint = startPoint + textLayout.getAscent()*1.1;//drop down a line
+        outline = textLayout.getOutline(transform); //get the outer edges of the text
+        //Draw a string on the image
+        //gImage.drawString(coords, 100, 100);//string does not quite line up if you use this, using fill instead.
+        gImage.setColor(Color.BLACK);
+        gImage.setStroke(new BasicStroke(10));
         gImage.draw(outline);//adds the outline
         gImage.setColor(Color.YELLOW);
         gImage.fill(outline); //fill in the outline with color
         
         //save image
         returnVal = fc.showOpenDialog(frame);
+        frame.getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
 		if (returnVal == JFileChooser.APPROVE_OPTION) {
             file = fc.getSelectedFile();
             //anything else we want to do with the file
